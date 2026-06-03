297749e56ccb764055f77457fb5dce4d685df579
mhadiwarraich-cell <m.hadi.warraich@gmail.com>
2026-06-03 18:15:30 +0100

    First Commit

---
+++ Print statement.py
@@ -0,0 +1,14 @@
+#Print a Sentence
+print("Welcome to the world of programming")
+
+#Print a Number
+print(4)
+
+#Next Line after print
+print("Hellow Python \n")
+
+#Pass multiple agrements for printing
+print("hello",5)
+
+#End argument of Print Statement
+print("welcome to",end= "*")
---
+++ Varieable.py
@@ -0,0 +1,13 @@
+# Creating different variables
+x = 7
+y = "Tony"
+print(x)
+print(y)
+
+Arsonel = "Arsonel"
+print(Arsonel)
+
+#User Input
+name = input("enter your name")
+
+print("\nHello",name, "\nwelcome to Codingal and Python")
