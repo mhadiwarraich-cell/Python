fb5e73729023f231187d41fd4cdd6b2de3cacfbd
M.Hadi <m.hadi.warraich@gmail.com>
2026-06-03 21:15:57 +0100

    Add files via upload

---
+++ Homework of Varieable.py
@@ -0,0 +1,15 @@
+
+# Creating different variables
+x = "Ahmad - December 24,2012"
+y = "Essa -  September 4,1016"
+z = "Elaf -  July 20,2020 "
+a = "Hadi - February 16,2015"
+b = "Amal - February 23,2026"
+
+print("Ahmad birthday:" , x)
+print("Essa birthday:" , y)
+print("Elaf birthday:" , z)
+print("Hadi birthday:" , a) 
+print("Amal birthaday:" , b)
+
+
