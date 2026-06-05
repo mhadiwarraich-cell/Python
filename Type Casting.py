acf0eda5345c73c3f496932347b3baf937a2f025
M.Hadi <m.hadi.warraich@gmail.com>
2026-06-05 18:09:29 +0100

    Add files via upload

---
+++ Type Casting.py
@@ -0,0 +1,27 @@
+# Assighining Different Verbials
+name = "M.Hadi"
+age = 11
+is_student = True
+weight = 40.000
+
+# Print Different Verbials and their Data Type
+print("Name :",name)
+print("Data type name is",type(name))
+
+print("Age :", age)
+print("Data Type of age is",type(age))
+
+print("is student :", is_student)
+print("Data Type of is_studentis", type(is_student))
+      
+print("weight :", weight)
+print("Data Type of weight is",type(weight))
+
+# Type casting to convert the datatype of verbials
+print("\n after type casting....")
+age = str(age)
+print(age)
+print("Data type of age is",type(age))
+weight = int(weight)
+print(weight)
+print("data type of weight is",type(weight))
