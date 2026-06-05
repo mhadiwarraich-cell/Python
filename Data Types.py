eb57a9cc602e953a564b26ce1b8ac825e7709f06
M.Hadi <m.hadi.warraich@gmail.com>
2026-06-05 18:09:12 +0100

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,12 @@
+# Let's cheack the Datatypes of values
+a=7
+print("type of a: ",type(a))
+
+b=16.03
+print("type of b: ",type(b))
+
+c="Arsonel"
+print("type of c: ",type(c))
+
+d= True
+print("type of d: ",type(d))
