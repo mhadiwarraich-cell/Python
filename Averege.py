5bf76626765a6a49169efd463d592ed8a2aaa88c
M.Hadi <m.hadi.warraich@gmail.com>
2026-06-09 17:40:50 +0100

    Add files via upload

---
+++ Averege.py
@@ -0,0 +1,15 @@
+# Storing Values
+mangox = 77
+juicea = 67
+crickety = 55
+footbalb = 3
+freefirec = 8
+hexord = 11
+
+ # Finding the total of mango,juice,cricket,footbal,freefire and hexor
+sum = mangox+juicea+crickety+footbalb+freefirec and hexord
+print("the sum of all the mangox,juicea+crickety,footbalb,freefirec and hexord is:",sum)
+
+# Finding the average of mango,juice+cricket,footbal,freefire and hexor
+average = sum/6
+print("the average of all the mango,juice+cricket,footbal,freefire and hexor is :", average)
