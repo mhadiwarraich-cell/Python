7cea03e07708b831e2a2c0f175c5be01cfb5f46b
M.Hadi <m.hadi.warraich@gmail.com>
2026-06-10 20:44:46 +0100

    Add files via upload

---
+++ Homwork of Summer times.py
@@ -0,0 +1,8 @@
+temperature = float(input("Enter the temperature (°C): "))
+
+if temperature < 15:
+    print("The weather is Cold mmmm.")
+elif temperature < 30:
+    print("The weather is Warmmmmmm.")
+else:
+    print("The weather is Hot hooootottt.")
