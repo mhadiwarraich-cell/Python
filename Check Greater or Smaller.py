29342a6fa58ddc16b872462a3548484368cd2c92
M.Hadi <m.hadi.warraich@gmail.com>
2026-06-10 18:01:47 +0100

    Add files via upload

---
+++ Check Greater or Smaller.py
@@ -0,0 +1,8 @@
+i = int(input("Enter a number to try me or!!! : "))
+if (i > 17):
+    print("i is smaller then 17")
+    print("i'm in if block")
+else:
+    print ("i is greater than 17")
+    print ("i'm in else block")
+print ("i'm not in if and not in else Block")
