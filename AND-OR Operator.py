50a8161a03ec0071f95eff7eaf1597fa72e237ae
M.Hadi <m.hadi.warraich@gmail.com>
2026-06-12 18:05:29 +0100

    Add files via upload

---
+++ AND-OR Operator.py
@@ -0,0 +1,25 @@
+x = 12
+y = 10
+h = 0
+
+if x,y and h:
+    print("All the numbers have boolean value as true")
+else:
+    print("Atleast one number hase boolean value as false")
+
+
+
+d = 7
+e = 5
+v = 6
+
+if d > 0 or e > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
+
+if v > 0 or h > 0:
+    print("Either of the number is greater than 0")
+else:
+    print("No number is greater than 0")
+
