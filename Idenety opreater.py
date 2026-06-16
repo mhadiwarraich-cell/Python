40f855f89bdad3d1689d195b304c29c6a68831e8
M.Hadi <m.hadi.warraich@gmail.com>
2026-06-16 17:36:42 +0100

    Add files via upload

---
+++ Idenety opreater.py
@@ -0,0 +1,42 @@
+# Python program to illustrate the use
+x = 7
+
+if (type(x) is int):
+
+    print("you are correct means true")
+
+else:
+
+    print("you are wrong means false")
+
+    
+h = 6.6
+
+if (type(h) is not float):
+
+    print("you are correct means true")
+
+else:
+
+    print("you are wrong means false")
+
+a = 70
+
+b = 80
+
+if (a is b):
+
+    print("a and b are the SAME idenity ok!")
+
+y = 50
+
+if (a is not y):
+
+    print("a and y have DIFFERENT idenety")
+
+
+
+
+
+    
+
