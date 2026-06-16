a609c7487241a165d6a8f42ae554758ce6d12edf
M.Hadi <m.hadi.warraich@gmail.com>
2026-06-16 18:02:29 +0100

    Add files via upload

---
+++ Bitwhiet opreater.py
@@ -0,0 +1,20 @@
+a = 20 
+
+b = -20
+
+
+# print bitwise right shift opreater
+
+print("a >> 1 =", a >> 1)
+
+print("b >> 1 =", b >> 1)
+
+x = 7
+
+y = 8
+
+# print bitwise left shift opreater
+
+print("x << 1 =", x << 1)
+
+print("y << 1 =", y << 1)
