17f66556fac22f645f59fcf80a27f42d3127dbdc
M.Hadi <m.hadi.warraich@gmail.com>
2026-06-17 17:45:08 +0100

    Add files via upload

---
+++ Digitable nimber.py
@@ -0,0 +1,9 @@
+print("Enter a number (Numenator): ")
+numn = int(input())
+print("Enter a Number (deniminator): ")
+numd = int(input())
+
+if numn%numd==0:
+    print("\n" +str(numn)+ " is dividsble by" +str(num4d))
+else:
+    print("\n"+str(numn) + " is not dividable by " +str(numd))
---
+++ Operater withsedence.py
@@ -0,0 +1,17 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0 
+z = (v+w) * x / y;
+print("Value of (v+w) * x/ y is ", z)
+
+
+
+name = "Hadi"
+age = 0
+
+if name == "Hadi" or name == "Ahmad" and age >= 2 :
+    print("Aslamualakum! hushamadid.")
+else : 
+    print("Allahafis phir mele gay!")
