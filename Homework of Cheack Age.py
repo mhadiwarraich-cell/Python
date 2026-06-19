5fefbd1d2942ead2bc80dfb06ea77aedb9e4863e
M.Hadi <m.hadi.warraich@gmail.com>
2026-06-19 18:53:23 +0100

    Add files via upload

---
+++ Homework of Cheack Age.py
@@ -0,0 +1,8 @@
+# Check Age
+
+age = int(input("Enter your age: "))
+
+if age >= 10 and age <= 20:
+    print("Your age is between 10 and 20 years.")
+else:
+    print("Your age is NOT between 10 and 20 years.")
