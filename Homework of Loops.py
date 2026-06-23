40b574c6a8b229b7dc8507a61a1d8cea276e2a5c
M.Hadi <m.hadi.warraich@gmail.com>
2026-06-23 20:02:59 +0100

    Add files via upload

---
+++ Homework of Loops.py
@@ -0,0 +1,8 @@
+# Power Calculator
+
+number = int(input("Enter a number: "))
+power = int(input("Enter the power: "))
+
+result = number ** power
+
+print(number, "raised to the power", power, "=", result)
