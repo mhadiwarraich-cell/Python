b8f0df89ed37f7fff2010da8e0873c1421ac516e
M.Hadi <m.hadi.warraich@gmail.com>
2026-06-26 18:02:00 +0100

    Add files via upload

---
+++ Is this a Prime Number.py
@@ -0,0 +1,16 @@
+#tkae two input from user 
+lower = int(input("enter a lower range: "))
+upper = int(input("enter a upprer range: "))
+
+print("Please numbers between", lower, "and", upper, "are:")
+#iterate loop  from lower limit to upper limit
+for num in range (lower, upper + 1 ):
+    # all pime numbers are greater than 1
+    if num > 1:
+        for i in range (2, num):
+             if (num % i) == 0:
+                 break
+             else:
+                 print(num)
+
+
