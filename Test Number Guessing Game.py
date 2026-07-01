40d2f738f440edb60aae0652268b23ecdae25c87
M.Hadi <m.hadi.warraich@gmail.com>
2026-07-01 18:21:44 +0100

    Add files via upload

--- Test Number Guessing Game.py
+++ Test Number Guessing Game.py
@@ -1,2 +1,23 @@
 print("Start your game")
 print("pick a secret number between 1 and 50. You have 5 attempts to guess it. ")
+
+
+a = secret = 278
+b = secret = 27
+c = secert = 56
+d = secert = 76
+e = secert = 89
+if not (a)("you lost"):
+   if not (b)("you lost"):
+    if not (c)("you lost"):
+        if not (d)("you lost"):
+             if not (e)("you lost"):
+                print("YOU HAVE 5 ATTEMPS")
+if not(a,b,c,d,e):print("you lost the game")
+else:
+    if all (a,b,c,d,e):
+        print("you won the game")
+    int(input("Enter the number"))   
+while "," (a,b,c,d,e) :
+    if not a ("your one incorrect"):
+        
