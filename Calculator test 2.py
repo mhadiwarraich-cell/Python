5921b662e37cb61666c9ccd0e636f0d34b844385
M.Hadi <m.hadi.warraich@gmail.com>
2026-07-15 18:10:42 +0100

    Add files via upload

---
+++ Calculator test 2.py
@@ -0,0 +1,22 @@
+print(" Welcome to the calculator")
+
+def add(a,bb):
+    return(a + bb)
+result = add(3, 4)
+
+        
+def subtract(a,bb):
+    return(a - bb)
+
+def multiply(a,bb):
+    return(a * bb)
+
+def  divide(a,bb):
+    return(a / bb)
+
+try:
+      add = int(input("Enter a number: "))
+except ValueError:
+    print
+    
+    float(input("The final answer is"))
