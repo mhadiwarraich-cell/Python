5e809518df4fb2b69cc17ce7298fc15b140c8c5c
M.Hadi <m.hadi.warraich@gmail.com>
2026-07-17 18:18:46 +0100

    Add files via upload

---
+++ Homework of trigonic value.py
@@ -0,0 +1,10 @@
+import math
+import random
+
+angle = random.randint(0, 360)
+rad = math.radians(angle)
+
+print("Angle:", angle)
+print("sin:", math.sin(rad))
+print("cos:", math.cos(rad))
+print("tan:", math.tan(rad))
