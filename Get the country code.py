8d5da1f5a00bac23a231b1e9158203627c61db41
M.Hadi <m.hadi.warraich@gmail.com>
2026-07-22 18:10:07 +0100

    Add files via upload

---
+++ Get the country code.py
@@ -0,0 +1,11 @@
+country_code = {'Pakistan' : '0015',
+                'Zerbhaijan' : '0016',
+                'Turkey' : '0017'}
+
+
+print("Country code of pakistan -")
+print(country_code.get('Pakistan', 'Not found'))
+
+
+print("country code for Azerbhaijan -")
+print("country_code.get('Azerbhaijan', 'Not found")
