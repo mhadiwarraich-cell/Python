c59f4202d6e09330c88849ed540c11e250120065
M.Hadi <m.hadi.warraich@gmail.com>
2026-07-28 18:49:01 +0100

    Add files via upload

---
+++ Homework of School Snack Counter.py
@@ -0,0 +1,15 @@
+# School Snack Counter
+
+snacks = {"Chips", "Juice", "Cookies"}
+snacks.add("Apple")
+
+friend = {"Apple", "Juice", "Cake"}
+print("Shared Snacks:", snacks.intersection(friend))
+
+counts = [5, 3, 2]
+counts.append(4)
+
+print("Number of 3s:", counts.count(3))
+
+counts.reverse()
+print("Reversed Counts:", counts)
