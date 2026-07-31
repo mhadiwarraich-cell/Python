270ee71b53fe64422970d453dce516ee7102f1ec
M.Hadi <m.hadi.warraich@gmail.com>
2026-07-31 18:08:30 +0100

    Add files via upload

---
+++ Pet class name.py
@@ -0,0 +1,23 @@
+class Pet:
+    name = "Pet"
+    
+    
+    def __init__(Self,name,animal,age,color):
+        Self.name = name
+        Self.animal = animal
+        Self.age = age
+        Self.color = color
+        
+A = Pet("Zoro","Dog", '11',"Light White")
+H = Pet("Tommy","Lion",'10', "Black")
+E = Pet("Siser", "Panther", '11', "White")
+F = Pet("Kuta", "Dog", '11', "Orange")
+C = Pet("sisy", "Cat", '7', "White")
+P = Pet("Cuty", "Cat", '6', "White")
+
+print(A.name,A.animal,A.age,A.color)
+print(H.name,H.animal,H.age,H.color)
+print(E.name,E.animal,E.age,E.color)
+print(F.name,F.animal,F.age,F.color)
+print(C.name,C.animal,C.age,C.color)
+print(P.name,P.animal,P.age,P.color)
