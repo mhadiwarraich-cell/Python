fc4b51ebe78958306b4746695df10cdfa50b15dc
M.Hadi <m.hadi.warraich@gmail.com>
2026-07-31 17:30:14 +0100

    Add files via upload

---
+++ Class Vehicle.py
@@ -0,0 +1,15 @@
+class Vehicle:
+    
+    
+    def __init__(self, max_speed, milage):
+        
+        
+        self.max_speed = max_speed
+        self.milage = milage
+        
+        
+modelX = Vehicle(240,18)
+
+
+print("Model Max Speed:", modelX.max_speed)
+print("Moadel Milage :", modelX.milage)
