d80ec76ac053bedddbbcd39de4d3078d19efc190
M.Hadi <m.hadi.warraich@gmail.com>
2026-08-07 17:19:54 +0100

    Add files via upload

---
+++ Keep It Private!.py
@@ -0,0 +1,21 @@
+class myClass:
+    
+    
+    __privateVar = 27;
+    
+    
+    
+    def __privMeth(self):
+        print("I'm inside class myclass")
+        
+        
+    def hello(self):
+        print("Private Variable value: ",myClass.__privateVar)
+    
+    
+    
+    
+    
+foo = myClass()
+foo.hello()
+foo.__privMeth
