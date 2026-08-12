84fd6be74d5af9dde0f7af4c91ce8e72cacc172a
M.Hadi <m.hadi.warraich@gmail.com>
2026-08-12 18:06:22 +0100

    Add files via upload

---
+++ Libarary systemc.py
@@ -0,0 +1,19 @@
+class Book:
+    def __init__(self, auther, title):
+        self.auther = auther
+        self.title = title
+        is_borrowed = False
+
+is_borrowed = True
+
+def borrow():
+    print("sets is_borrowed to True")
+    print("It is confermed")
+    
+def return_book():
+    print(" sets is_borrowed to False ")
+    print("It is Confermed")
+    
+    A = 
+    B =
+    C = 
