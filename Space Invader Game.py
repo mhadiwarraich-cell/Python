84692939512ef1c54461694151723bc3dab51228
M.Hadi <m.hadi.warraich@gmail.com>
2026-08-31 20:00:28 +0100

    Add files via upload

--- Space Invader Game.py
+++ Space Invader Game.py
@@ -11,7 +11,7 @@ ENEMY_START_Y_MIN = 50
 ENEMY_START_Y_MAX = 150
 ENEMY_SPEED_X = 4
 ENEMY_SPEED_Y = 40
-BULLET_SPEED_Y = 10
+BULLET_SPEED_Y = 10 
 COLLISION_DISTANCE = 27
 
 # Initialize Pygame
@@ -30,6 +30,7 @@ pygame.display.set_icon(icon)
 
 # Player
 playerImg = pygame.image.load('player1.png')
+playerImg = pygame.transform.scale(playerImg, (200, 200))
 playerX = PLAYER_START_X
 playerY = PLAYER_START_Y
 playerX_change = 0
@@ -43,20 +44,23 @@ enemyY_change = []
 num_of_enemies = 6
 
 for _i in range(num_of_enemies):
-    enemyImg.append(pygame.image.load('enemy.png'))
-    enemyX.append(random.randint(0, SCREEN_WIDTH - 64))  # 64 is the size of the enemy
+    enemy = pygame.image.load('enemy.png')
+    enemy = pygame.transform.scale(enemy, (150, 150))
+    enemyImg.append(enemy)
+    enemyX.append(random.randint(0, SCREEN_WIDTH - 64))
     enemyY.append(random.randint(ENEMY_START_Y_MIN, ENEMY_START_Y_MAX))
     enemyX_change.append(ENEMY_SPEED_X)
     enemyY_change.append(ENEMY_SPEED_Y)
 
+
 # Bullet
 bulletImg = pygame.image.load('bullet.png')
+bulletImg = pygame.transform.scale(bulletImg, (10, 20))
 bulletX = 0
 bulletY = PLAYER_START_Y
 bulletX_change = 0
 bulletY_change = BULLET_SPEED_Y
 bullet_state = "ready"
-
 # Score
 score_value = 0
 font = pygame.font.Font('freesansbold.ttf', 32)
