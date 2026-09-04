3d9c998c0ddfd2bb76d7dded9ba0a6950f9f250f
M.Hadi <m.hadi.warraich@gmail.com>
2026-09-04 17:40:28 +0100

    Add files via upload

---
+++ Event Handler.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+
+
+window = Tk()
+window.title("Event Handler")
+window.geometry("100x100")
+
+
+def handle_keypress(event):
+    """Print the chracter associated to the key pressed"""
+    print(event.char)
+    
+    
+window.bind("<Key>", handle_keypress)
+
+
+def handle_click(event):
+    print("\3nThe button was clicked!")
+    
+button = Button(text="Click me!")
+button.pack()
+
+button.bind("<Button->", handle_click)
+
+
+window.mainloop()
